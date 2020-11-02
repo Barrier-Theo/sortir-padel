f67971163b1211d4056fdca9061c37887f3a3cc6
Maxime Varron <varron.maxime@gmail.com>
2020-11-02 12:15:23 +0100

    ajout bootstrap + crud

---
+++ src/Form/LieuType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Lieu;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LieuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('rue')
+            ->add('latitude')
+            ->add('longitude')
+            ->add('Ville', EntityType::class,[
+                'class' => Ville::class,
+                'choice_label' => 'nom'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Lieu::class,
+        ]);
+    }
+}
