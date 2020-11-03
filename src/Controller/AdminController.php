20d1d507dbfc88b223d5cb10d40aad47895b55f8
Maxime Varron <varron.maxime@gmail.com>
2020-11-03 12:02:44 +0100

    ajout import par csv

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Form\ImportType;
+use App\Repository\CampusRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdminController extends AbstractController{
 
@@ -24,20 +29,16 @@ class AdminController extends AbstractController{
     /**
      * @Route("/admin/import", name="admin_import")
      */
-    public function import(Request $request): Response
+    public function import(Request $request, CampusRepository $campusRepository, EntityManagerInterface $em
+    , UserPasswordEncoderInterface $passwordEncoder)
     {
         $form = $this->createForm(ImportType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $csvFile = $form['csv']->getData();
-
-              // If a file is uploaded
               if ($csvFile) {
-                  // this is needed to safely include the file name as part of the URL
-                  $newFilename = 'importUser'.$csvFile->guessExtension();
-                
-                  // Move the file to the directory where brochures are stored
+                  $newFilename = 'importUser.txt';
                   try {
                       $csvFile->move(
                           //Path parameter is in "services.yaml"
@@ -45,16 +46,58 @@ class AdminController extends AbstractController{
                           $newFilename
                       );
                   } catch (FileException $e) {
-                    // handle exception if something happens during file upload and redirecting to the page
                     $this->addFlash('error', 'Erreur pendant l\'import du fichier');
-                    $this->redirectToRoute('admin');
+                    return $this->redirectToRoute('admin');
                   }             
               }
-              // Call import form TraitementImport
               
-              //$import->import($pathName, $form);
-              //Redirect to admin page
-              //$this->redirectToRoute('admin.import');
+        $pathCSV = $this->getParameter('csv_directory');
+        if (($handle = fopen($pathCSV.'/importUser.txt', 'r')) !== FALSE) {
+
+            
+                //On boucle sur chaque ligne du csv
+                while (($data = fgetcsv($handle, 600, ";")) !== FALSE) {
+                    if(!(strlen($data[0])> 0 && strlen($data[0]) < 255) ||
+                        !(strlen($data[1])> 0 && strlen($data[1]) < 255) ||
+                        !(strlen($data[2])> 0 && strlen($data[2]) < 255) ||
+                        !(strlen($data[3])> 0 && strlen($data[3]) < 15) ||
+                        !(strlen($data[4])> 0 && strlen($data[4]) < 255) ||
+                        !(strlen($data[5])> 0 && strlen($data[5]) < 255) ||
+                        !($data[6] == 0 || $data[6]==1) ||
+                        !($data[7] == 0 || $data[7]==1) ){
+                        $this->addFlash('error', 'le fichier csv ne respecte pas le standard d\'import');
+                        return $this->redirectToRoute('admin_import');
+                    }
+                    if($campusRepository->findOneBy(['nom' => $data[8]])== null){
+                        $this->addFlash('error', 'le campus : ' .$data[8].'n\'existe pas');
+                        return $this->redirectToRoute('admin_import');
+                    }
+                }
+        }
+        if (($handle = fopen($pathCSV.'/importUser.txt', 'r')) !== FALSE) {
+                //On boucle sur chaque ligne du csv
+                while (($data = fgetcsv($handle, 600, ";")) !== FALSE) {
+                    $campus = $campusRepository->findOneBy(['nom' => $data[8]]);
+                    if($campus != null){
+                        $participant = new Participant();
+                        $participant->setNom($data[1])
+                                ->setPrenom($data[2])
+                                ->setTelephone($data[3])
+                                ->setMail($data[4])
+                                ->setAdministrateur($data[6])
+                                ->setActif($data[7])
+                                ->setCampus($campus);
+                        $participant->setMotDePasse($passwordEncoder->encodePassword(
+                                    $participant,
+                                    $data[5]
+                                ));
+                    $em->persist($participant);
+                    }
+                }
+                $em->flush();
+                $this->addFlash('success', 'Import réussi');
+                unlink($pathCSV.'/importUser.txt');
+            } 
         }
         return $this->render('importAdmin.html.twig', [
             'form' => $form->createView(),
